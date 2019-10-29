8d6231b736b4ca31e883d97f05e97f0225ef638f
Giovana <giovanasilveiracardoso@gmail.com>
2019-10-28 22:08:34 -0300

    Criação do endpoint de delete, put e get id do condomínio

--- condominio/services.py
+++ condominio/services.py
@@ -1,3 +1,5 @@
+from rest_framework.exceptions import NotFound
+
 from .models import Condominio
 
 
@@ -14,4 +16,23 @@ class CondominioService:
 
         condominio.save()
 
-        return condominio
+    def update(self, params):
+        condominio_id = params['id']
+        cnpj = params['cnpj']
+        endereco = params['endereco']
+        nome = params['nome']
+
+        condominio = Condominio(id=condominio_id, cnpj=cnpj, endereco=endereco, nome=nome)
+        condominio.save(update_fields=['cnpj', 'endereco', 'nome'])
+
+    def delete(self, condominio_id):
+        try:
+            Condominio.objects.get(id=condominio_id).delete()
+        except Condominio.DoesNotExist:
+            raise NotFound(detail='Condomínio não encontrado')
+
+    def find_by_id(self, condominio_id):
+        try:
+            return Condominio.objects.get(id=condominio_id)
+        except Condominio.DoesNotExist:
+            raise NotFound(detail='Condomínio não encontrado')
