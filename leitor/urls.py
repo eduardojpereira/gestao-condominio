8f7e59ffb425e8bc42b2e4439610abbed816cd01
Lucas Souza <31324149+LucSouza@users.noreply.github.com>
2019-10-31 20:05:04 -0300

    retirada linha duplicada

--- leitor/urls.py
+++ leitor/urls.py
@@ -3,9 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.LeitorView.as_view(), name='leitores')
-=======
-    path('', views.LeitorView.as_view, name='leitores')
->>>>>>> 549ab3d2d6364d17f923887e1639f78f1d344584
 ]
