24c1e5073b67fc8c61a40348094e5832fb2093c3
Danilo A. Almeida <danilo.a.almeida@live.jp>
2019-11-04 17:48:20 -0300

    ajuste model consumo

--- consumo/models.py
+++ consumo/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from apartamento.models import Apartamento
+from datetime import datetime
 
 # Create your models here.
 class Consumo(models.Model):
 
-    id = models.AutoField(primary_key=True) #
-    apartamento = Apartamento #
-    data_leitura_atual = models.DateField() #
-    leitura_atual = models.FloatField() #
-    data_leitura_anterior = ''
-    periodo_leitura = '' #
-    consumo_atual = 0 #
-    valor_gas = models.FloatField() #
-    valor_pagamento = '' #
+    id = models.AutoField(primary_key=True)
+    apartamento = models.ForeignKey(Apartamento, on_delete=models.CASCADE())
+    leitura_atual = models.DecimalField()
+    leitura_anterior = models.DecimalField()
+    data_leitura_atual = models.DateField()
+    data_leitura_anterior = models.DateField()
+    consumo_atual = models.DecimalField()
+    valor_gas = models.DecimalField()
+    valor_pagamento = models.DecimalField()
 
     class Meta:
         db_table = 'consumo'
@@ -20,9 +21,27 @@ class Consumo(models.Model):
     def __str__(self):
         string = 'Apartamento: ' + str(self.apartamento) + '\n' \
                     'Data da Leitura: ' + str(self.data_leitura_atual) + '\n' \
-                     'Leitura Atual: ' + str(self.data_leitura_atual) + 'm³ \n' \
-                     'Período da Leitura: ' + str(self.periodo_leitura) + '\n' \
+                     'Leitura Atual: ' + str(self.leitura_atual) + 'm³ \n' \
                      'Consumo Atual: ' + str(self.consumo_atual) + '\n' \
                      'Valor do Gás: R$' + str(self.valor_gas) + '/kg \n' \
                      'Valor a Pagar: R$' + str(self.valor_gas) + '\n'
         return string
+
+    def _periodo_leitura(self):
+        data_leitura_atual = datetime.strptime(self.data_leitura_atual,'%d-%m-%Y')
+        data_leitura_anterior = datetime.strptime(self.data_leitura_anterior, '%d-%m-%Y')
+        return abs((data_leitura_atual - data_leitura_anterior).days)
+
+    def _consumo_atual(self):
+        leitura_atual = self.leitura_atual
+        leitura_anterior = self.leitura_anterior
+        consumo = (leitura_atual - leitura_anterior)
+        return consumo
+
+    def _valor_pagamento(self):
+        consumo_atual = self.consumo_atual
+        valor_gas = self.valor_gas
+        unidade_conversao = 2.3
+        consumo_em_kg = consumo_atual * unidade_conversao
+        valor_pagamento = consumo_em_kg * valor_gas
+        return valor_pagamento
